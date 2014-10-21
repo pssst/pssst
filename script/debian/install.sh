310eaba9c15ccf508bf6d6a1dbaea2357176e563
Christian Uhsat <christian@uhsat.de>
2014-10-21 10:01:19 +0200

    Changed Debian install script

--- script/debian/install.sh
+++ script/debian/install.sh
@@ -4,6 +4,16 @@ set -o nounset
 
 URL=https://raw.github.com/pssst/pssst/master/app/cli/pssst.py
 
+echo "This script will install the Pssst CLI on your system."
+
+while true; do
+    read -p "Do you want to continue? [y/n] " ANSWER
+    case $ANSWER in
+        [Yy]* ) break;;
+        [Nn]* ) exit 1;;
+    esac
+done
+
 if [[ $EUID == 0 ]]; then
     DIR=/usr/local/bin
 else
@@ -13,5 +23,5 @@ fi
 wget -O $DIR/pssst $URL > /dev/null 2>&1
 chmod 755 $DIR/pssst
 
-echo "Installed in $DIR"
+echo "Pssst installed in $DIR"
 exit 0
