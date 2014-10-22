d3ed14b9fb6195f8a89cbb2690f9f23b23201fa3
Christian Uhsat <christian@uhsat.de>
2014-10-22 14:52:16 +0200

    Fixed incompatible install script

--- script/debian/install.sh
+++ script/debian/install.sh
@@ -4,16 +4,6 @@ set -o nounset
 
 URL=https://raw.github.com/pssst/pssst/master/app/cli/pssst.py
 
-echo "This script will install the Pssst CLI on your system."
-
-while true; do
-    read -p "Do you want to continue? [y/n] " ANSWER
-    case $ANSWER in
-        [Yy]* ) break;;
-        [Nn]* ) exit 1;;
-    esac
-done
-
 if [[ $EUID == 0 ]]; then
     DIR=/usr/local/bin
 else
