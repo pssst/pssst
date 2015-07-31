eebc85d9709ed7e334b22619fa510c05e6728ab9
Christian Uhsat <christian@uhsat.de>
2015-07-31 20:41:00 +0200

    Fixed padding error

--- app/cli/pssst.py
+++ app/cli/pssst.py
@@ -245,7 +245,7 @@ class Pssst:
         def encrypt(self, data):
             nonce = Random.get_random_bytes(Pssst._Key.NONCE_SIZE)
             size = AES.block_size - len(data) % AES.block_size
-            data = data.ljust(size, chr(size))
+            data = data.ljust(AES.block_size, size)
 
             data = AES.new(nonce[:32], AES.MODE_CBC, nonce[32:]).encrypt(data)
             nonce = PKCS1_OAEP.new(self.key).encrypt(nonce)
